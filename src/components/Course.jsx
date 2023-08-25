acd0f2889bc8f24f47c8678dfa24b2f3c70a8c29
Tomi Halko <tomi.halko@gmail.com>
2023-08-25 13:52:28 +0300

    Tehtävä 2.4 ja 2.5

---
+++ src/components/Course.jsx
@@ -0,0 +1,20 @@
+const Course = ({ courses }) => {
+
+  const elements = courses.map(course => {
+    return (
+      <div key={course.id}>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <p>Number of exercises {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+      </div>
+    )
+  })
+
+  return <div>{elements}</div>
+}
+
+const Header = ({ course }) => <h1>{course}</h1>
+const Content = (props) => <>{props.parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}</>
+const Part = ({ part, exercises }) => <p>{part} {exercises}</p>
+
+export default Course
